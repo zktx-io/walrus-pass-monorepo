8a56b0fad20dbdd769c4de77771110cfddffbb8f
Lim Jet <57783762+daoauth@users.noreply.github.com>
2024-10-21 16:23:04 +0900

    Update config.ts

--- packages/holder/src/utils/config.ts
+++ packages/holder/src/utils/config.ts
@@ -1,5 +1,5 @@
 export const FILE_NAME_ACCOUNT = 'account.json';
 export const FILE_NAME_DID_DOCS = 'did-docs.json';
 export const REDIRECT_URL =
-  'https://4yju9x2wv34m3zpqg5dt1eaxvor0d0wpvett3rmebzy6xxrdvh.walrus.site';
+  'https://gpjeai8yynewxai2jmkr0qvhudze6mjhv9ulwypqpsoljx99j.walrus.site/auth';
 export const SALT_TEMP = '1553493906625353164421699608680';
